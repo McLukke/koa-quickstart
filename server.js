71881422ff1fe8fd34aa147a75ce6eb8cf56dbcd
Kenny Lu <klu@realmatters.com>
2017-04-27 11:42:22 -0400

    add if statements for diff routes / give 404 status / create middleware fn

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
 var koa = require('koa');
-var app = koa();
+var app = new koa();
+
+var requestTime = function(headerName) {
+  return function *(next) {
+    var start = new Date();
+    yield next;
+    var end = new Date();
+    var ms = end - start;
+    this.set(headerName, ms + 'ms');
+  }
+}
+
+app.use(requestTime('Response-time'));
+
+app.use(function *(){
+  console.log(this.request);
+
+  var url = this.request.url;
+  if (url === '/') {
+    this.body = 'Hello from koajs';
+  } else if (url === '/date') {
+    this.body = new Date();
+  } else {
+    this.status = 404;
+    this.body = 'Sorry, I don\'t know what you want'
+  }
+});
+
+app.listen(4000);
